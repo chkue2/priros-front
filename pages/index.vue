a608f68c0422c728bc14196d82bf5dc1f91e6b63
최한규 <hangyu@choehangyuui-Macmini.local>
2023-10-11 17:16:54 +0900

    update index.vue & package add sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,39 @@
 <template>
-  <NuxtPage />
+  <section class="main-section min-w-1250">
+    <p class="main-title">업무 협업의 미래를 열다</p>
+    <p class="main-text-contents">
+      혁신적인 솔루션으로, 업무 협업의 미래를 현실로 만나보세요.<br>
+      업무 효율성을 향상하고 무료 마켓 사용료로 더욱 경제적으로 업무를 관리하세요.<br>
+      사례비 영업이 필요 없는 저희와 함께 업무 협업을 새로운 수준에서 경험해보세요.
+    </p>
+    <section class="check-your-today">
+      <p class="check-your-today-title">CHECK YOUR TODAY</p>
+    </section>
+  </section>
 </template>
+<style scoped lang="scss">
+  .main-title{
+    margin: 150px 0 50px;
+    font-size: 38px;
+    line-height: 16px;
+    font-weight: 700;
+    text-align: center;
+  }
+  .main-text-contents {
+    font-size: 18px;
+    line-height: 40px;
+    text-align: center;
+    color: #6b6b6b;
+  }
+  .check-your-today{
+    height: 613px;
+    background-color: #4c4f59;
+    .check-your-today-title {
+      font-size: 24px;
+      line-height: 36px;
+      font-weight: 700;
+      color: #ffffff;
+      padding: 32px 0 56px;
+    }
+  }
+</style>
