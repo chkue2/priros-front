15144e7cbbe1948ec51ca15404442d60ef656c2d
hangyu <chkue0924@lawendtech.co.kr>
2023-12-28 16:51:09 +0900

    약관 페이지 생성

---
+++ packages/mo/pages/terms/personal.vue
@@ -0,0 +1,7 @@
+<template>
+  <PersonalTerm />
+</template>
+
+<script setup>
+import PersonalTerm from '@priros/common/components/card/PersonalTerm.vue'
+</script>
---
+++ packages/mo/pages/terms/service.vue
@@ -0,0 +1,7 @@
+<template>
+  <ServiceTerm />
+</template>
+
+<script setup>
+import ServiceTerm from '@priros/common/components/card/ServiceTerm.vue'
+</script>
