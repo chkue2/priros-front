ca481e969bbc2e6d0ab2131d4c3060771c8446cd
hangyu <chkue0924@lawendtech.co.kr>
2024-01-08 10:36:09 +0900

    pc layout 중간저장

---
+++ packages/pc/assets/js/menus.js
@@ -0,0 +1,104 @@
+const menus = [
+  {
+    title: '마켓 의뢰',
+    submenus: [
+      {
+        icon: 'matching.svg',
+        title: '매칭',
+        to: '/'
+      },
+      {
+        icon: 'process.svg',
+        title: '진행',
+        to: '/'
+      },
+      {
+        icon: 'success.svg',
+        title: '완료',
+        to: '/'
+      },
+    ]
+  },
+  {
+    title: '마켓 수임',
+    submenus: [
+      {
+        icon: 'matching.svg',
+        title: '매칭',
+        to: '/'
+      },
+      {
+        icon: 'process.svg',
+        title: '진행',
+        to: '/'
+      },
+      {
+        icon: 'success.svg',
+        title: '완료',
+        to: '/'
+      },
+    ]
+  },
+  {
+    title: '설정대리인',
+    submenus: [
+      {
+        icon: 'check-box-gray.svg',
+        title: '설정업무',
+        to: '/'
+      }
+    ]
+  },
+  {
+    title: '마이페이지',
+    submenus: [
+      {
+        icon: 'peoples-gray.svg',
+        title: '회원정보 / 사용자정보',
+        to: '/'
+      },
+      {
+        icon: 'face-id-gray.svg',
+        title: '사용자관리',
+        to: '/'
+      },
+      {
+        icon: 'setting-gray-1.svg',
+        title: '환경설정',
+        to: '/'
+      },
+      {
+        icon: 'card-add-gray.svg',
+        title: '결제등록 및 관리',
+        to: '/'
+      },
+      {
+        icon: 'graduation-cap-gray.svg',
+        title: '협약 전문가 관리',
+        to: '/'
+      },
+      {
+        icon: 'edit-contained-gray.svg',
+        title: '일대일 문의',
+        to: '/'
+      },
+    ]
+  },
+  {
+    title: '고객센터',
+    submenus: [
+      {
+        icon: 'headphones-gray.svg',
+        title: '공지사항',
+        to: '/'
+      },
+      {
+        icon: 'help-square-contained-gray.svg',
+        title: '헬프센터',
+        to: '/'
+      },
+    ]
+  },
+]
+
+export { menus }
