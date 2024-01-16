a7505a5ae24551a8d99f6a86257b727efb1028d8
hangyu <chkue0924@lawendtech.co.kr>
2024-01-16 09:10:45 +0900

    아이디 찾기 및 비밀번호 찾기 api 연동

--- packages/mo/pages/user/find/id.vue
+++ packages/mo/pages/user/find/id.vue
@@ -4,21 +4,36 @@
     <div class="join-form">
       <p class="join-form-title">이름 *</p>
       <div class="join-form-input-container">
-        <input type="text" class="join-form-input" placeholder="이름을 입력하세요" >
+        <input v-model="form['name']" type="text" class="join-form-input" placeholder="이름을 입력하세요" >
       </div>
       <p class="join-form-title">휴대전화번호 *</p>
       <div class="join-form-input-container">
-        <input type="tel" class="join-form-input" placeholder="-없이 숫자만 입력하세요" >
+        <input v-model="form['mobileNo']" type="tel" class="join-form-input" placeholder="-없이 숫자만 입력하세요" >
       </div>
     </div>
   </div>
   <div class="join-bottom-buttons sticky">
-    <CommonBottomButton id="generalApplyButton" width="100%" text="아이디 찾기" height="60px" />
+    <CommonBottomButton id="generalApplyButton" width="100%" text="아이디 찾기" height="60px" @click="handlerClickApplyButton" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import CommonBottomButton from '@priros/common/components/button/CommonBottomButton.vue'
+
+import { user } from '~/services/user.js'
+
+const form = ref({})
+
+const handlerClickApplyButton = () => {
+  user.findId(form.value)
+    .then(() => {
+      alert('SMS로 아이디가 발송되었습니다.')
+    })
+    .catch(e => {
+      alert(e.response.data.message)
+    })
+}
 </script>
 
 <style lang="scss" scoped>
