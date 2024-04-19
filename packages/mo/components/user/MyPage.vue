d9fb159b84dbf9594d3ae1faa9b0e500a9bd5888
remagine2 <remagine@naver.com>
2024-04-19 16:12:18 +0900

    구버전 보기 추가

--- packages/mo/components/user/MyPage.vue
+++ packages/mo/components/user/MyPage.vue
@@ -77,8 +77,7 @@ const profile = computed(() => auth.user.profile);
 const menuItems = ref(defaultMypageMenu);
 
 function oldVersion() {
-  document.cookie = 'priros_version=1; SameSite=None; Secure; domain=.dev.priros.com';
-  window.location.href = 'https://dev.priros.com';
+  window.location.href = 'https://dev.priros.co.kr?priros_version=1';
 }
 
 function logout() {
