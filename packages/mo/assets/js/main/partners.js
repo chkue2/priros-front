3349edc37e8162ef69288514822cc6fd5b956ed2
hangyu <chkue0924@lawendtech.co.kr>
2024-05-03 16:00:44 +0900

    메인 파트너 이미지 변경

--- packages/mo/assets/js/main/partners.js
+++ packages/mo/assets/js/main/partners.js
@@ -2,8 +2,8 @@ const partners = [
   '/img/main/kbank.png',
   '/img/main/kakaobank.png',
   '/img/main/bnk-02.png',
-  '/img/main/jbbank.png',
-  '/img/main/kjbank.png',
+  '/img/main/jbbank-01.png',
+  '/img/main/kjbank-01.png',
 ]
 
 export { partners }
