f401f5fd909494adabb020d6e0a465daf664f7ee
hangyu <chkue0924@lawendtech.co.kr>
2024-08-29 10:50:18 +0900

    [카카오 뱅크] M750 전문 개발

--- packages/mo/pages/case/[id]/reception-report.vue
+++ packages/mo/pages/case/[id]/reception-report.vue
@@ -6,7 +6,7 @@
           <div class="forms">
             <div class="form-group">
               <div class="label">
-                <label for="" class="form-label">접수일</label>
+                <label class="form-label">접수일<b>*</b></label>
               </div>
               <div class="form-input">
                 <input v-model="receiveDate" type="date" readonly />
@@ -14,7 +14,7 @@
             </div>
             <div class="form-group">
               <div class="label">
-                <label for="" class="form-label">접수번호</label>
+                <label class="form-label">접수번호<b>*</b></label>
                 <span class="form-warning"
                   >해당 명의인의 접수번호를 입력해주세요</span
                 >
@@ -28,6 +28,26 @@
                   maxlength="20"
                 />
               </div>
+            </div>
+            <div v-if="allowFileUpload === 'Y'" class="form-group">
+              <div class="label">
+                <label class="form-label">파일첨부</label>
+              </div>
+              <div class="form-input">
+                <input
+                  ref="fileList"
+                  type="file"
+                  accept=".gif, .jpg, .jpeg, .png, .pdf, .bmp, .tif, .tiff"
+                  @change="handlerChangeFileList"
+                />
+                <p class="input-file mt" @click="handlerClickFileList">
+                  {{ filePreviewName }}
+                  <img src="/img/icon/file-gray.png" alt="" />
+                </p>
+              </div>
+              <p class="form-help-text">
+                매매계약서상 변경내용이 있을 경우 매매계약서를 첨부해주세요.
+              </p>
             </div>
           </div>
         </div>
@@ -58,6 +78,7 @@
 <script setup>
 import { onMounted, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import dayjs from "@priros/common/node_modules/dayjs";
 
 import CommonBottomButton from "@priros/common/components/button/CommonBottomButton.vue";
 import CommonAlertModal from "@priros/common/components/modal/CommonAlertModal.vue";
@@ -70,14 +91,20 @@ definePageMeta({
   layout: false,
 });
 
+const router = useRouter();
+const alertStore = useAlertStore();
+
 const tradeCaseId = useRoute().params.id;
 
 const receiveNo = ref("");
 const receiveDate = ref("");
 const receiveBuyer = ref("");
+const receiveFiles = ref(null);
+const fileList = ref(null);
+const changedContractFileList = ref([]);
+const allowFileUpload = ref("N");
 
-const router = useRouter();
-const alertStore = useAlertStore();
+const isSuccessModalShow = ref(false);
 
 onMounted(() => {
   tradeCaseReceptionReport
@@ -88,6 +115,8 @@ onMounted(() => {
       }
       receiveBuyer.value = data.buyer;
       receiveNo.value = data.receiveNo;
+      changedContractFileList.value = data.changedContractFileList;
+      allowFileUpload.value = data.allowFileUpload;
     })
     .catch((e) => {
       alertStore.open(e.response.data.message);
@@ -101,8 +130,33 @@ watch(
     receiveNo.value = onlyNumber(receiveNo.value);
   }
 );
+const filePreviewName = computed(() => {
+  return receiveFiles.value !== null
+    ? receiveFiles.value[0].name
+    : changedContractFileList.value.length === 1
+      ? `[${changedContractFileList.value[0].docName}] ${dayjs(changedContractFileList.value[0].created).format("YYYY-MM-DD")}`
+      : "파일첨부하기";
+});
 
-const isSuccessModalShow = ref(false);
+const handlerClickFileList = () => {
+  fileList.value.click();
+};
+const handlerChangeFileList = (e) => {
+  if (e.target.files.length === 0) return false;
+  const files = Array.from(e.target.files);
+  if (files.length > 1) {
+    alertStore.open("파일은 한번에 최대 5개까지만 첨부 가능합니다.");
+    return false;
+  }
+  for (let file of files) {
+    if (file.size > 10 * 1024 * 1024) {
+      alertStore.open("10MB 이상의 파일은 첨부할 수 없습니다.");
+      return false;
+    }
+  }
+  receiveFiles.value = files;
+};
+
 const toggleSuccessModal = () => {
   isSuccessModalShow.value = !isSuccessModalShow.value;
 };
@@ -113,11 +167,22 @@ const handleBtnSendClick = () => {
     return false;
   }
 
+  const formData = new FormData();
+  // formData.append("receiveDate", receiveDate.value);
+  // formData.append("receiveNo", receiveNo.value);
+
+  if (receiveFiles.value !== null && receiveFiles.value.length > 0) {
+    receiveFiles.value.forEach((file) => {
+      formData.append("changedContractFileList", file);
+    });
+  }
+
   tradeCaseReceptionReport
-    .post(tradeCaseId, {
-      receiveDate: receiveDate.value,
-      receiveNo: receiveNo.value,
-    })
+    .post(
+      tradeCaseId,
+      { receiveDate: receiveDate.value, receiveNo: receiveNo.value },
+      formData
+    )
     .then(() => {
       toggleSuccessModal();
     })
@@ -129,4 +194,14 @@ const handleBtnSendClick = () => {
 
 <style scoped lang="scss">
 @import "@priros/common/assets/scss/views/dialog";
+.form-label {
+  b {
+    color: #dc6024;
+  }
+}
+.form-help-text {
+  font-size: 11px;
+  color: #3c323f;
+  margin-top: 4px;
+}
 </style>
--- packages/mo/services/tradeCaseReceptionReport.js
+++ packages/mo/services/tradeCaseReceptionReport.js
@@ -1,12 +1,26 @@
-import {API_URL, GET_AUTH, POST_AUTH, getEndpoint} from "~/composables/useApi.js";
+import {
+  API_URL,
+  GET_AUTH,
+  POST_AUTH,
+  getEndpoint,
+  getEndpointOfRepay,
+} from "~/composables/useApi.js";
 
 export const tradeCaseReceptionReport = {
   async get(tradeCaseId) {
-    const endpoint = getEndpoint(API_URL.TRADE_CASE.RECEIVE, {trade_case_id: tradeCaseId})
-    return await GET_AUTH(endpoint)
+    const endpoint = getEndpoint(API_URL.TRADE_CASE.RECEIVE, {
+      trade_case_id: tradeCaseId,
+    });
+    return await GET_AUTH(endpoint);
   },
-  async post(tradeCaseId, data) {
-    const endpoint = getEndpoint(API_URL.TRADE_CASE.RECEIVE, {trade_case_id: tradeCaseId})
-    return await POST_AUTH(endpoint, data)
+  async post(tradeCaseId, data, formData) {
+    const endpoint = getEndpointOfRepay(
+      API_URL.TRADE_CASE.RECEIVE,
+      {
+        trade_case_id: tradeCaseId,
+      },
+      data
+    );
+    return await POST_AUTH(endpoint, formData);
   },
-}
+};
